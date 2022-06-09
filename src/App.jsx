f3fccc6b4fd023f9795750a6b066d0297356b11a
darcyTwo <3538356136@qq.com>
2022-06-09 18:07:02 +0800

    react-router

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Route, Switch,Redirect} from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import MyNavLink from './components/MyNavLink'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="row">
+            <div className="col-xs-offset-2 col-xs-8">
+              <div className="page-header"><h2>React Router Demo</h2></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-2 col-xs-offset-2">
+              <div className="list-group">
+                {/* 在React中靠路由链接实现切换组件 */}
+                <MyNavLink to="/about">about</MyNavLink>
+                <MyNavLink to="/home">home</MyNavLink>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <div className="panel">
+                <div className="panel-body">
+                  {/* 注册路由 */}
+                  <Switch>
+                    <Route path="/about" component={About} />
+                    {/* 开启精准匹配--exact */}
+                    <Route path="/home" component={Home} />
+                    {/* 路由的重定向 */}
+                    <Redirect to="/about"/>
+                  </Switch>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class 我是Home组件 extends Component {
+  render() {
+    return (
+      <div>我是Home组件</div>
+    )
+  }
+}
